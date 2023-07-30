3f57560dab2524213b20f5ceb96588c916cc852b
sergio15dev <sergio15dev@gmail.com>
2023-07-30 10:50:05 -0500

    chat microservice

--- backend/services/chat/controllers/chat.controllers.js
+++ backend/services/chat/controllers/chat.controllers.js
@@ -1,5 +1,19 @@
+/**
+ * Módulo de manejo de mensajes en un chat de red social.
+ * @module chatHandlers
+ */
+
 import Message from "../models/Message.js";
 
+/**
+ * Obtiene todos los mensajes de chat disponibles.
+ * @function getChats
+ * @async
+ * @param {Object} req - Objeto de solicitud de Express.
+ * @param {Object} res - Objeto de respuesta de Express.
+ * @returns {Object} - Devuelve una lista de mensajes de chat en formato JSON.
+ * @throws {Error} - Si ocurre algún error en el servidor.
+ */
 export const getChats = async (req, res) => {
   try {
     const messages = await Message.find();
@@ -10,10 +24,18 @@ export const getChats = async (req, res) => {
   }
 };
 
+/**
+ * Crea un nuevo mensaje y lo agrega a la base de datos.
+ * @function createMessage
+ * @async
+ * @param {Object} req - Objeto de solicitud de Express.
+ * @param {Object} res - Objeto de respuesta de Express.
+ * @returns {Object} - Devuelve el mensaje creado en formato JSON con el código de estado 201 (CREATED).
+ * @throws {Error} - Si ocurre algún error en el servidor.
+ */
 export const createMessage = async (req, res) => {
   try {
     const { sender, receiver, content } = req.body;
-    // Aquí puedes validar y procesar los datos recibidos antes de crear el mensaje
     const newMessage = new Message({
       sender,
       receiver,
@@ -28,6 +50,16 @@ export const createMessage = async (req, res) => {
   }
 };
 
+/**
+ * Elimina un mensaje específico por su ID de la base de datos.
+ * @function deleteMessage
+ * @async
+ * @param {Object} req - Objeto de solicitud de Express.
+ * @param {Object} res - Objeto de respuesta de Express.
+ * @returns {Object} - Devuelve un mensaje de éxito en formato JSON con el código de estado 200 (OK) si el mensaje se elimina correctamente.
+ * @throws {Error} - Si el mensaje no se encuentra, devuelve un mensaje de error en formato JSON con el código de estado 404 (NOT FOUND).
+ * @throws {Error} - Si ocurre algún error en el servidor.
+ */
 export const deleteMessage = async (req, res) => {
   try {
     const { id } = req.params;
@@ -45,6 +77,15 @@ export const deleteMessage = async (req, res) => {
   }
 };
 
+/**
+ * Obtiene una conversación específica por el ID del remitente o receptor.
+ * @function getConversation
+ * @async
+ * @param {Object} req - Objeto de solicitud de Express.
+ * @param {Object} res - Objeto de respuesta de Express.
+ * @returns {Object} - Devuelve una lista de mensajes de chat que coinciden con el ID del remitente o receptor en formato JSON.
+ * @throws {Error} - Si no se encuentran mensajes, devuelve un mensaje de error en formato JSON con el código de estado 500 (INTERNAL SERVER ERROR).
+ */
 export const getConversation = async (req, res) => {
   try {
     const { id } = req.params;
@@ -62,4 +103,3 @@ export const getConversation = async (req, res) => {
     return res.status(500).json({ message: "Error en el servidor" });
   }
 };
-
